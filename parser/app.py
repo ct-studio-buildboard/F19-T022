2942f96c60ac3c4c0f6ffa7795672a06c1797b0a
Ryan Kim <kimryan0416@gmail.com>
2019-11-06 14:50:50 -0500

    Prototype parser w/ python

---
+++ parser/app.py
@@ -0,0 +1,29 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+def GetContents(url):
+	uf = urllib.request.urlopen(url)
+	html = uf.read()
+	soup = BeautifulSoup(html, features="lxml")
+
+	# kill all script and style elements
+	for script in soup(["script", "style"]):
+		# rip it out
+		script.extract()    
+
+	# get text
+	text = soup.get_text()
+
+	# break into lines and remove leading and trailing space on each
+	lines = (line.strip() for line in text.splitlines())
+	# break multi-headlines into a line each
+	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+	# drop blank lines
+	text = '\n'.join(chunk for chunk in chunks if chunk)
+
+	return text
+
+if __name__ == "__main__":
+	url = "https://www.ryankimdev.com"
+	t = GetContents(url)
+	print(t)
