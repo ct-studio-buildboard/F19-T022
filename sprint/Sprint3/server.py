2ce3dba3eac285e3736ed3fbdcd8d17128d449e2
Emilie <Emilie@dhcp-nyc-1458.redrover.cornell.edu>
2019-11-21 18:16:52 -0500

    update

--- sprint/Sprint3/server.py
+++ sprint/Sprint3/server.py
@@ -5,42 +5,11 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST',])
 
+def parse_request():
+	data = request.data
+	return data 
 
-def GetContents(url):
-	uf = urllib.request.urlopen(url)
-	html = uf.read()
-	soup = BeautifulSoup(html, features="lxml")
+app.run(debug=True)
 
-	# kill all script and style elements
-	for script in soup(["script", "style"]):
-		# rip it out
-		script.extract()    
-
-	# get text
-	text = soup.get_text()
-
-	# break into lines and remove leading and trailing space on each
-	lines = (line.strip() for line in text.splitlines())
-	# break multi-headlines into a line each
-	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-	# drop blank lines
-	text = '\n'.join(chunk for chunk in chunks if chunk)
-
-	return text
-
-
-
-def main():
-
-	@app.route(url, methods = ['GET', 'POST',])
-
-	GetContents(url)
-	
-    app.run(host='0.0.0.0', debug=True, port=3134)
-
-
-
-if __name__=='__main__':
-    main()
